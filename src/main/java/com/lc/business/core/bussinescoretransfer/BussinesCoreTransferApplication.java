e697eda3ad546f8334e28ee70ffc2cb58c1d2306
Jose Juape <josejuapeq@bcp.com.pe>
2019-09-15 22:06:06 -0500

    Project Structure

---
+++ src/main/java/com/lc/business/core/bussinescoretransfer/BussinesCoreTransferApplication.java
@@ -0,0 +1,13 @@
+package com.lc.business.core.bussinescoretransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BussinesCoreTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BussinesCoreTransferApplication.class, args);
+	}
+
+}
